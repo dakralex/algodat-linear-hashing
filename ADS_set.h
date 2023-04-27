62e1df8de1655d0295dfa9887f10a4b4a6d60951
Daniel Kral <dakral@protonmail.com>
2023-04-27 18:38:09 +0200

    Prepare ADS_set.h for linear hashing: Remove unneeded attributes and comment out methods and classes for phase 2

--- ADS_set.h
+++ ADS_set.h
@@ -6,10 +6,10 @@
 #include <iostream>
 #include <stdexcept>
 
-template <typename Key, size_t N = /* default N (implementation-defined) */>
+template <typename Key, size_t N = 1>
 class ADS_set {
 public:
-  class /* iterator type (implementation-defined) */;
+  // class /* iterator type (implementation-defined) */;
   using value_type = Key;
   using key_type = Key;
   using reference = value_type &;
@@ -18,63 +18,62 @@ public:
   using difference_type = std::ptrdiff_t;
   using const_iterator = /* iterator type */;
   using iterator = const_iterator;
-  using key_compare = std::less<key_type>;                         // B+-Tree
-  using key_equal = std::equal_to<key_type>;                       // Hashing
-  using hasher = std::hash<key_type>;                              // Hashing
+  using key_equal = std::equal_to<key_type>;
+  using hasher = std::hash<key_type>;
 
-  ADS_set();                                                           // PH1
-  ADS_set(std::initializer_list<key_type> ilist);                      // PH1
-  template<typename InputIt> ADS_set(InputIt first, InputIt last);     // PH1
-  ADS_set(const ADS_set &other);
+  ADS_set();
+  ADS_set(std::initializer_list<key_type> ilist);
+  template<typename InputIt> ADS_set(InputIt first, InputIt last);
+  // ADS_set(const ADS_set &other);
 
-  ~ADS_set();
+  // ~ADS_set();
 
-  ADS_set &operator=(const ADS_set &other);
-  ADS_set &operator=(std::initializer_list<key_type> ilist);
+  // ADS_set &operator=(const ADS_set &other);
+  // ADS_set &operator=(std::initializer_list<key_type> ilist);
 
-  size_type size() const;                                              // PH1
-  bool empty() const;                                                  // PH1
+  size_type size() const;
+  bool empty() const;
 
-  void insert(std::initializer_list<key_type> ilist);                  // PH1
+  void insert(std::initializer_list<key_type> ilist);
   std::pair<iterator,bool> insert(const key_type &key);
-  template<typename InputIt> void insert(InputIt first, InputIt last); // PH1
+  template<typename InputIt> void insert(InputIt first, InputIt last);
 
-  void clear();
-  size_type erase(const key_type &key);
+  // void clear();
+  // size_type erase(const key_type &key);
 
-  size_type count(const key_type &key) const;                          // PH1
-  iterator find(const key_type &key) const;
+  size_type count(const key_type &key) const;
+  // iterator find(const key_type &key) const;
 
-  void swap(ADS_set &other);
+  // void swap(ADS_set &other);
 
-  const_iterator begin() const;
-  const_iterator end() const;
+  // const_iterator begin() const;
+  // const_iterator end() const;
 
-  void dump(std::ostream &o = std::cerr) const;
+  // void dump(std::ostream &o = std::cerr) const;
 
-  friend bool operator==(const ADS_set &lhs, const ADS_set &rhs);
-  friend bool operator!=(const ADS_set &lhs, const ADS_set &rhs);
+  // friend bool operator==(const ADS_set &lhs, const ADS_set &rhs);
+  // friend bool operator!=(const ADS_set &lhs, const ADS_set &rhs);
 };
 
-template <typename Key, size_t N>
-class ADS_set<Key,N>::/* iterator type */ {
-public:
-  using value_type = Key;
-  using difference_type = std::ptrdiff_t;
-  using reference = const value_type &;
-  using pointer = const value_type *;
-  using iterator_category = std::forward_iterator_tag;
+// template <typename Key, size_t N>
+// class ADS_set<Key,N>::/* iterator type */ {
+// public:
+//   using value_type = Key;
+//   using difference_type = std::ptrdiff_t;
+//   using reference = const value_type &;
+//   using pointer = const value_type *;
+//   using iterator_category = std::forward_iterator_tag;
+//
+//   explicit /* iterator type */(/* implementation-dependent */);
+//   reference operator*() const;
+//   pointer operator->() const;
+//   /* iterator type */ &operator++();
+//   /* iterator type */ operator++(int);
+//   friend bool operator==(const /* iterator type */ &lhs, const /* iterator type */ &rhs);
+//   friend bool operator!=(const /* iterator type */ &lhs, const /* iterator type */ &rhs);
+// };
 
-  explicit /* iterator type */(/* implementation-dependent */);
-  reference operator*() const;
-  pointer operator->() const;
-  /* iterator type */ &operator++();
-  /* iterator type */ operator++(int);
-  friend bool operator==(const /* iterator type */ &lhs, const /* iterator type */ &rhs);
-  friend bool operator!=(const /* iterator type */ &lhs, const /* iterator type */ &rhs);
-};
-
-template <typename Key, size_t N>
-void swap(ADS_set<Key,N> &lhs, ADS_set<Key,N> &rhs) { lhs.swap(rhs); }
+// template <typename Key, size_t N>
+// void swap(ADS_set<Key,N> &lhs, ADS_set<Key,N> &rhs) { lhs.swap(rhs); }
 
 #endif // ADS_SET_H
